3427aafb89a9b8e0c5d31b13c1909f0f96392001
kelvin373-ht <kelvin373.ht@gmail.com>
2020-04-13 22:19:22 +0700

    Add ViewPager for MoneyFragment, GopayFragment, and OvoFragment

--- app/src/main/java/com/algokelvin/n_pocket/MainActivity.kt
+++ app/src/main/java/com/algokelvin/n_pocket/MainActivity.kt
@@ -2,11 +2,23 @@ package com.algokelvin.n_pocket
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.tabs.TabLayout
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val pocketViewPageAdapter = AppsViewPageAdapter(supportFragmentManager)
+        pocketViewPageAdapter.addAppsPage(MoneyFragment(), GopayFragment(), OvoFragment())
+        layoutViewPager.adapter = pocketViewPageAdapter
+
+        tabs_layout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabReselected(p0: TabLayout.Tab?) { }
+            override fun onTabUnselected(p0: TabLayout.Tab?) { }
+            override fun onTabSelected(p0: TabLayout.Tab?) { layoutViewPager.setCurrentItem(p0?.position!!, true) }
+        })
     }
 }
