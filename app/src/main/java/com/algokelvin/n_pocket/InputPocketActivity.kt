3a05532e8b6e83b451e9fec6f5842a0b32d70aeb
kelvin373-ht <kelvin373.ht@gmail.com>
2020-04-20 21:59:56 +0700

    Action edit text for Date Picker Calendar

--- app/src/main/java/com/algokelvin/n_pocket/InputPocketActivity.kt
+++ app/src/main/java/com/algokelvin/n_pocket/InputPocketActivity.kt
@@ -1,9 +1,12 @@
 package com.algokelvin.n_pocket
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.DatePickerDialog
+import android.app.DatePickerDialog.OnDateSetListener
 import android.os.Bundle
-import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_input_pocket.*
+import java.util.*
+
 
 class InputPocketActivity : AppCompatActivity() {
 
@@ -11,7 +14,20 @@ class InputPocketActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_input_pocket)
 
-        val typePocket = intent.getStringExtra("type_pocket_data")
-        titleInput.text = typePocket
+        val typePocket = intent.getStringExtra("type_pocket_data") ?: "money"
+        val title = getString(R.string.input_note_pocket, typePocket.substring(0, 1).toUpperCase(Locale.getDefault())+typePocket.substring(1))
+
+        titleInput.text = title
+
+        data_date.setOnClickListener {
+            val calender = Calendar.getInstance()
+            val day: Int = calender.get(Calendar.DAY_OF_MONTH)
+            val month: Int = calender.get(Calendar.MONTH)
+            val year: Int = calender.get(Calendar.YEAR)
+            val picker = DatePickerDialog(this, OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+                data_date.setText(dayOfMonth.toString() + "/" + (monthOfYear + 1) + "/" + year)
+            }, year, month, day)
+            picker.show()
+        }
     }
 }
