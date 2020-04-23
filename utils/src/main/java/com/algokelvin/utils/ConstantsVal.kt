91cae9bd4e01a294c7eca6aca667c6c87978ff32
kelvin373-ht <kelvin373.ht@gmail.com>
2020-04-24 00:17:53 +0700

    Set Date in every type Pocket

--- utils/src/main/java/com/algokelvin/utils/ConstantsVal.kt
+++ utils/src/main/java/com/algokelvin/utils/ConstantsVal.kt
@@ -1,2 +1,18 @@
 package com.algokelvin.utils
 
+import java.text.SimpleDateFormat
+import java.util.*
+
+fun getDate(dateFormat: String): String {
+    val date = Calendar.getInstance().time
+    val formatDate = SimpleDateFormat(dateFormat, Locale.getDefault())
+    return formatDate.format(date)
+}
+fun getDateConvert(oldFormat: String, oldDate: String, newFormat: String): String {
+    val sdf = SimpleDateFormat(oldFormat, Locale.ENGLISH)
+    sdf.timeZone = TimeZone.getTimeZone("UTC")
+    val date = sdf.parse(oldDate)
+    sdf.timeZone = TimeZone.getDefault()
+    sdf.applyPattern(newFormat)
+    return sdf.format(date)
+}
