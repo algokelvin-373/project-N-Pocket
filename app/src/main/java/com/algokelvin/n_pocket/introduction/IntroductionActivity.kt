ea7e5329915c6e0d9f5d856ababb8b1e6198f327
kelvin373-ht <kelvin373.ht@gmail.com>
2020-04-25 21:04:40 +0700

    Success UI Layout Introduction

--- app/src/main/java/com/algokelvin/n_pocket/introduction/IntroductionActivity.kt
+++ app/src/main/java/com/algokelvin/n_pocket/introduction/IntroductionActivity.kt
@@ -2,6 +2,7 @@ package com.algokelvin.n_pocket.introduction
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.algokelvin.n_pocket.AppsViewPageAdapter
 import com.algokelvin.n_pocket.R
 import kotlinx.android.synthetic.main.activity_introduction.*
@@ -19,10 +20,14 @@ class IntroductionActivity : AppCompatActivity() {
         text_back.setOnClickListener {
             if (itemPage > 0) itemPage--
             layoutViewPagerIntroduction.setCurrentItem(itemPage, true)
+            if (itemPage != 2) text_next.visibility = View.VISIBLE
+            if (itemPage == 0) text_back.visibility = View.GONE
         }
         text_next.setOnClickListener {
             if (itemPage < 2) itemPage++
             layoutViewPagerIntroduction.setCurrentItem(itemPage, true)
+            if (itemPage == 2) text_next.visibility = View.GONE
+            if (itemPage != 0) text_back.visibility = View.VISIBLE
         }
     }
 }
--- app/src/main/java/com/algokelvin/n_pocket/introduction/IntroductionOne.kt
+++ app/src/main/java/com/algokelvin/n_pocket/introduction/IntroductionOne.kt
@@ -1,12 +1,6 @@
 package com.algokelvin.n_pocket.introduction
 
-import android.os.Bundle
-import android.view.View
 import com.algokelvin.n_pocket.R
 import com.algokelvin.utils.layout.IntroductionFragment
 
-class IntroductionOne : IntroductionFragment(R.layout.fragment_introduction_one) {
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-    }
-}
+class IntroductionOne : IntroductionFragment(R.layout.fragment_introduction_one)
